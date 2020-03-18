7d68cbd0054fac6cb3254eac85dcac9e6a5fd0fd
Spencer Nelson <spencernelson144@gmail.com>
2020-03-17 22:25:04 -0600

    completed change

---
+++ change/solution.py
@@ -0,0 +1,50 @@
+def _get_change_making_matrix(set_of_coins, r: int):
+    m = [[0 for _ in range(r + 1)] for _ in range(len(set_of_coins) + 1)]
+    for i in range(1, r + 1):
+        m[0][i] = float('inf')  # By default there is no way of making change
+    return m
+
+def change_making(coins, n: int):
+    """This function assumes that all coins are available infinitely.
+    n is the number to obtain with the fewest coins.
+    coins is a list or tuple with the available denominations.
+    """
+    m = _get_change_making_matrix(coins, n)
+    print(m)
+    for c in range(1, len(coins) + 1):
+        for r in range(1, n + 1):
+            # Just use the coin coins[c - 1].
+            if coins[c - 1] == r:
+                m[c][r] = 1
+            # coins[c - 1] cannot be included.
+            # Use the previous solution for making r,
+            # excluding coins[c - 1].
+            elif coins[c - 1] > r:
+                m[c][r] = m[c - 1][r]
+            # coins[c - 1] can be used.
+            # Decide which one of the following solutions is the best:
+            # 1. Using the previous solution for making r (without using coins[c - 1]).
+            # 2. Using the previous solution for making r - coins[c - 1] (without
+            #      using coins[c - 1]) plus this 1 extra coin.
+            else:
+                m[c][r] = min(m[c - 1][r], 1 + m[c][r - coins[c - 1]])
+            print(m)
+            print('*******')
+    
+    return m[-1][-1]
+
+
+# coins = [1, 5, 10, 25, 100]
+# value = 15
+# coins = [1, 4, 15, 20, 50]
+# value = 23
+# coins = [1, 5, 10, 25]
+# value = 1
+
+
+coins = [2,5,10,20,50]
+value = 21
+
+result2 = change_making(coins, value)
+
+print(result2)
